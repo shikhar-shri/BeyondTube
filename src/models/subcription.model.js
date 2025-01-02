c5e3c5fa01c8e4946192228cf71f0df7fc0fb0ad
shikhar <shikhar.sri23@gmail.com>
2025-01-02 16:30:55 +0530

    added subscription model

---
+++ src/models/subcription.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    channel: {
+      required: true,
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    subscriber: [
+      {
+        type: Schema.Types.ObjectId, //subscriber subscribes to channel
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Subscription = new model("Subscription", subscriptionSchema);
